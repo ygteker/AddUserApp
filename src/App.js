d734aa0b1aa11803d61ae69f3fae3bc949413fb2
Yagiz Gunes Teker <30394534+ygteker@users.noreply.github.com>
2022-01-01 15:55:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: username, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
